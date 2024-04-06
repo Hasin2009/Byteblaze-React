67f511e18ab6e3f4edb87d9529e5865d4274581b
hasinsadap <sdapananda72@gmail.com>
2024-04-06 17:38:51 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1>This is Home</h1>
+        </div>
+    );
+};
+
+export default Home;
